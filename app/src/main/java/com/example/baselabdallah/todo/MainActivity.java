c0a1fbb9b05cb5873ff2952ebbec1f65da9d579f
Basel Abdallah <baselabdallah@Basels-MacBook-Pro.local>
2017-01-04 21:12:23 +0200

    Build a custom adapter

--- app/src/main/java/com/example/baselabdallah/todo/MainActivity.java
+++ app/src/main/java/com/example/baselabdallah/todo/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.baselabdallah.todo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
+    /**
+     * Varaible representing the list view filled with todo objects
+     */
+    private ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.listview);
+        //Filling the array with testing text
+        final ListViewItem[] items = new ListViewItem[10];
+        for(int i=0; i<items.length; i++){
+            items[i] = new ListViewItem("Test");
+        }
+        //Binding the array with the list view through the custom adapter
+        CustomAdapter customAdapter = new CustomAdapter(this, R.id.text, items);
+        listView.setAdapter(customAdapter);
+
     }
 }
